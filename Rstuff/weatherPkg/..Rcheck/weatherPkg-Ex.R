a7d133654bf8c203109952a7ce792887b7e9c037
unknown <angel.zhou@hutchinhill.com>
2013-06-24 15:37:21 -0400

    Added all the new files

---
+++ Rstuff/weatherPkg/..Rcheck/weatherPkg-Ex.R
@@ -0,0 +1,69 @@
+pkgname <- "weatherPkg"
+source(file.path(R.home("share"), "R", "examples-header.R"))
+options(warn = 1)
+options(pager = "console")
+library('weatherPkg')
+
+base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
+cleanEx()
+nameEx("read")
+### * read
+
+flush(stderr()); flush(stdout())
+
+### Name: read
+### Title: Reads tables
+### Aliases: read
+
+### ** Examples
+
+weather2 = read("./inst/extdata/link1.txt", FALSE, 1, 3)
+
+weather3 = read("link2.txt", TRUE, 1, 2)
+
+
+
+cleanEx()
+nameEx("readWeather1")
+### * readWeather1
+
+flush(stderr()); flush(stdout())
+
+### Name: readWeather1
+### Title: Reads tables
+### Aliases: readWeather1
+
+### ** Examples
+
+weather1 = readWeather1("weather.txt", TRUE, "%b/%Y/%d")
+
+
+
+cleanEx()
+nameEx("weatherPkg-package")
+### * weatherPkg-package
+
+flush(stderr()); flush(stdout())
+
+### Name: weatherPkg-package
+### Title: What the package does (short line) ~~ package title ~~
+### Aliases: weatherPkg-package weatherPkg
+### Keywords: package
+
+### ** Examples
+
+~~ simple examples of the most important functions ~~
+
+
+
+### * <FOOTER>
+###
+options(digits = 7L)
+base::cat("Time elapsed: ", proc.time() - base::get("ptime", pos = 'CheckExEnv'),"\n")
+grDevices::dev.off()
+###
+### Local variables: ***
+### mode: outline-minor ***
+### outline-regexp: "\\(> \\)?### [*]+" ***
+### End: ***
+quit('no')
---
+++ Rstuff/weatherPkg/R/readWeather1.R
@@ -0,0 +1,23 @@
+#' Reads tables
+#'
+#' Creates a data frame with desirable data from data tables formatted
+#' the way it is in link 1.
+#'
+#' @param fileName Name of the text file
+#' @param h Decides if the header is kept or not
+#' @param dateFormat The way that the dates are formatted
+#'
+#' @export
+#' @examples
+#'
+#' weather1 = readWeather1("weather.txt", TRUE, "%b/%Y/%d")
+
+readWeather1 <- function(fileName, h, dateFormat) {
+    dframe = read.table(fileName, header = h)
+    mframe = melt(dframe, id = "Year")
+    stringDates <- paste(mframe$variable, mframe$Year, 20, sep="/")
+    Date = as.Date(stringDates, format=dateFormat)
+    Temperature <- mframe$value
+    cleanFrame <- data.frame(Date, Temperature)
+    return(cleanFrame)
+}
