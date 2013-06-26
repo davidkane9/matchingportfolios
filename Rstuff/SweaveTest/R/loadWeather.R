ba653b8c1795d587bc394973d758aee44ae02406
Angel <angellz557@gmail.com>
2013-06-26 13:45:05 -0400

    deleted stuff

--- Rstuff/SweaveTest/R/loadWeather.R
+++
@@ -1,9 +0,0 @@
-loadWeather <-
-function() {
-    x <- read.table("weather.txt", header = TRUE)
-    y <- melt(x, id="Year", )
-    stringDates <- paste(y$variable, y$Year, 20, sep="/")
-    y$Date <- as.Date(stringDates, format="%b/%Y/%d")
-    y$Temperatures <- y$value
-    return(y)
-}
