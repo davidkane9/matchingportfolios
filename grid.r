d2b3a7ed8eb97e4e08f21ce2c84f478fdae3aacc
mjfwilliams <mjf2@williams.edu>
2013-07-10 12:30:37 -0400

    updated sample.r

---
+++ grid.r
@@ -0,0 +1,7 @@
+vp = function(x,y) viewport(layout.pos.row = x, layout.pos.col = y)
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(1, 2, heights = unit(c(0.5, 5, 5, 5), "null"))))   
+plot1 = ggplot(points, aes(GE, IBM)) + geom_point()
+print(plot1, vp = vp(1,1))
+plot2 = ggplot(points, aes(GE, KO)) + geom_point()
+print(plot2, vp = vp(1,2))
