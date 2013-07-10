c855a2675b94695bd71498fb73f1608dd981a33a
Angel <angellz557@gmail.com>
2013-07-10 13:16:01 -0400

    added modified code

--- plots.R
+++ plots.R
@@ -10,46 +10,90 @@ b = 1
 set.seed(40)
 
 w = getWeights.hnr(A = Amat, b = b, n = 1000)
+points = as.data.frame(t(w))
+colnames(points) = c("GE", "IBM", "KO")
 
 library(rgl)
+scatterTheme1 = theme(
+  panel.background = NULL,
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  axis.title.x = element_blank(),
+  axis.ticks.x = element_blank(),
+  axis.text.x = element_blank(),
+  plot.margin = unit(c(0.1,0.1,0.1,0.1), "lines")
+)
 
-d <- as.data.frame(t(w))
-p1 <- ggplot(d, aes(w[1,], w[3,]))
-p1 <- p1 + xlab("GE")
-p1 <- p1 + ylab("KO")
-p1 <- p1 + geom_point(size = .1)
-##p1 <- p1 + theme(plot.margin = unit(0, "grobwidth"))
-p1
+scatterTheme2 = theme(
+  panel.background = NULL,
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  plot.margin = unit(c(0.1,0.1,0.1,0.1), "lines")
+)
 
-p2 <- ggplot(d, aes(w[1,], w[2,]))
-p2 <- p2 + xlab("GE")
-p2 <- p2 + ylab("IBM")
-p2 <- p2 + geom_point(size = .1)
-p2
+scatterTheme3 = theme(
+  panel.background = NULL,
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  axis.title.y = element_blank(),
+  axis.ticks.y = element_blank(),
+  axis.text.y = element_blank(),
+  plot.margin = unit(c(0.1,0.5,0.1,0.1), "lines")
+)
 
-p3 <- ggplot(d, aes(w[2,], w[3,]))
-p3 <- p3 + xlab("IBM")
-p3 <- p3 + ylab("KO")
-p3 <- p3 + geom_point(size = .1)
-p3
+histTheme1 = theme(
+  panel.background = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.border = element_blank(),
+  axis.title = element_blank(),
+  axis.ticks = element_blank(),
+  axis.text = element_blank(),
+  plot.margin = unit(c(0.1,0.05,0.1,1.5), "lines")
+)
 
-h1 <- ggplot(d, aes(w[1,]))
-h1 <- h1 + geom_histogram(binwidth = .125/4)
-h1 <- h1 + xlab("GE")
-h1 <- h1 + ylab("Counts")
-h1
+histTheme2 = theme(
+  panel.background = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.border = element_blank(),
+  axis.title = element_blank(),
+  axis.ticks = element_blank(),
+  axis.text = element_blank(),
+  plot.margin = unit(c(0.1,0.1,0.1,0.1), "lines")
+)
 
-h2 <- ggplot(d, aes(w[2,]))
-h2 <- h2 + geom_histogram(binwidth = .125/4)
-h2 <- h2 + xlab("IBM")
-h2 <- h2 + ylab("Counts")
-h2
+histTheme3 = theme(
+  panel.background = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.border = element_blank(),
+  axis.title.y = element_blank(),
+  axis.ticks.y = element_blank(),
+  axis.text.y = element_blank(),
+  plot.margin = unit(c(0.1,0.1,0.1,0.1), "lines")
+)
 
-h3 <- ggplot(d, aes(w[3,]))
-h3 <- h3 + geom_histogram(binwidth= .125/4)
-h3 <- h3 + xlab("KO")
-h3 <- h3 + ylab("Counts")
-h3
+p1 <- ggplot(data = points, aes(x = GE, y = KO))
+p1 <- p1 + geom_point(size = .1) + scatterTheme2
+
+p2 <- ggplot(data = points, aes(x = GE, y = IBM))
+p2 <- p2 + geom_point(size = .1) + scatterTheme1
+
+p3 <- ggplot(data = points, aes(x = IBM, y = KO))
+p3 <- p3 + geom_point(size = .1) + scatterTheme3
+
+h1 <- ggplot(data = points, aes(x = GE))
+h1 <- h1 + geom_histogram(binwidth = .125/4, fill = "white", color = "black")
+h1 <- h1 + ylab("Counts") + histTheme1
+
+h2 <- ggplot(data = points, aes(x = IBM))
+h2 <- h2 + geom_histogram(binwidth = .125/4, fill = "white", color = "black")
+h2 <- h2 + ylab("Counts") + histTheme2
+
+h3 <- ggplot(data = points, aes(x = KO))
+h3 <- h3 + geom_histogram(binwidth = .125/4, fill = "white", color = "black")
+h3 <- h3 + ylab("Counts") + histTheme3
 
 library(wq)
 layOut(list(h1, 1, 1), list(p2, 2, 1), list(p1, 3, 1), list(h2, 2, 2), list(p3, 3, 2), list(h3, 3, 3))
