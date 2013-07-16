b138aeb8d9a63d727195d2614361a8a64d2162ac
mjfwilliams <mjf2@williams.edu>
2013-07-16 10:51:27 -0400

    added test cases

--- randPort/R/Internals.R
+++
@@ -1,9 +0,0 @@
-
-.dummy <- function(vec) {
-  names = sort(unique(vec))
-  mat = matrix(rep(0, length(vec)*length(names)), nrow = length(names))
-  for(i in 1:nrow(mat)) {
-    mat[i,][which(vec == names[i])] = 1
-  }
-  return(mat)
-}
--- randPort/R/overlap.R
+++ randPort/R/overlap.R
@@ -39,3 +39,39 @@ setMethod("overlap", signature = signature(object = "randPort"),
 
 
         })
+
+overlap <- function(mat, orig = NULL, compare = FALSE) {
+  
+  correlations = cor(mat)
+  cat("Overlap:\n");
+  cat("Mean Correlaton:  ", paste(mean(correlations[lower.tri(correlations)])), "\n")
+  cat("Sd Correlations:  ", paste(sd(correlations[lower.tri(correlations)])), "\n\n")
+  
+  dists = apply(mat, 2, function(m) {
+    return(apply(mat, 2, function(x) sqrt(sum((x-m)^2))))    
+  })
+  cat("Mean Distance:    ", paste(mean(dists[lower.tri(dists)])), "\n")
+  cat("Sd Distance:      ", paste(sd(dists[lower.tri(dists)])), "\n\n")
+  
+  cat("Average weight:   ", paste(mean(mat)), "\n\n")
+  
+  if(compare) {
+    cat("Compare to Uniform on Scaled Simplex:\n")
+    matU = matrix(rexp(nrow(mat)*ncol(mat)), ncol = ncol(mat), nrow = nrow(mat))
+    matU = apply(matU, 2, function(x) x/sum(x)*sum(mat[,1]))
+    mat = matU
+    correlations = cor(mat)
+    
+    cat("Mean Correlaton:  ", paste(mean(correlations[lower.tri(correlations)])), "\n")
+    cat("Sd Correlations:  ", paste(sd(correlations[lower.tri(correlations)])), "\n\n")
+    
+    dists = apply(mat, 2, function(m) {
+      return(apply(mat, 2, function(x) sqrt(sum((x-m)^2))))    
+    })
+    cat("Mean Distance:    ", paste(mean(dists[lower.tri(dists)])), "\n")
+    cat("Sd Distance:      ", paste(sd(dists[lower.tri(dists)])), "\n\n")
+    
+    cat("Average weight:   ", paste(mean(mat)), "\n\n")
+  }
+  
+}
